d097e81722a40f676250b3ec3557610bb786fc02
Aimer Neige <aimer.neige.soft@gmail.com>
2023-02-23 21:47:19 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# MiraiGo-module-tarot
+
+ID: `com.aimerneige.tarot`
+
+Module for [MiraiGo-Template](https://github.com/Logiase/MiraiGo-Template)
+
+## 功能
+
+抽取塔罗牌。
+
+## 指令
+
+- `塔罗` `塔罗牌` 输出提示信息
+- `运势预测` 单张牌预测运势
+- `塔罗占卜` 三张牌进行占卜
+- `塔罗5` 抽取制定张数的塔罗牌
+
+## 使用方法
+
+在适当位置引用本包
+
+```go
+package example
+
+imports (
+    // ...
+
+    _ "github.com/yukichan-bot-module/MiraiGo-module-tarot"
+
+    // ...
+)
+
+// ...
+```
+
+## LICENSE
+
+<a href="https://www.gnu.org/licenses/agpl-3.0.en.html">
+<img src="https://www.gnu.org/graphics/agplv3-155x51.png">
+</a>
+
+本项目使用 `AGPLv3` 协议开源，您可以在 [GitHub](https://github.com/yukichan-bot-module/MiraiGo-module-tarot) 获取本项目源代码。为了整个社区的良性发展，我们强烈建议您做到以下几点：
+
+- **间接接触（包括但不限于使用 `Http API` 或 跨进程技术）到本项目的软件使用 `AGPLv3` 开源**
+- **不鼓励，不支持一切商业使用**
---
+++ tarot.go
@@ -0,0 +1,191 @@
+package tarot
+
+import (
+	"bytes"
+	"embed"
+	"io"
+	"io/fs"
+	"math/rand"
+	"path"
+	"strconv"
+	"sync"
+
+	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Logiase/MiraiGo-Template/utils"
+	"github.com/Mrs4s/MiraiGo/client"
+	"github.com/Mrs4s/MiraiGo/message"
+)
+
+//go:embed assets/deck
+var deckEmbedFS embed.FS
+
+//go:embed assets/tarot.jpg
+var tarotCommandImg []byte
+
+var instance *tarot
+var logger = utils.GetModuleLogger("com.aimerneige.tarot")
+
+type tarot struct {
+}
+
+func init() {
+	instance = &tarot{}
+	bot.RegisterModule(instance)
+}
+
+func (t *tarot) MiraiGoModule() bot.ModuleInfo {
+	return bot.ModuleInfo{
+		ID:       "com.aimerneige.tarot",
+		Instance: instance,
+	}
+}
+
+// Init 初始化过程
+// 在此处可以进行 Module 的初始化配置
+// 如配置读取
+func (t *tarot) Init() {
+}
+
+// PostInit 第二次初始化
+// 再次过程中可以进行跨 Module 的动作
+// 如通用数据库等等
+func (t *tarot) PostInit() {
+}
+
+// Serve 注册服务函数部分
+func (t *tarot) Serve(b *bot.Bot) {
+	b.GroupMessageEvent.Subscribe(func(c *client.QQClient, msg *message.GroupMessage) {
+		msgString := msg.ToString()
+		if len(msgString) < 6 {
+			return
+		}
+		if msgString == "塔罗牌" || msgString == "塔罗" {
+			replyMsg := simpleText("支持指令\n「运势预测」（单张牌预测运势）\n「塔罗占卜」（三张牌进行占卜）\n「塔罗5」（抽取制定张数的塔罗牌）")
+			img, err := uploadImage(c, msg.GroupCode, bytes.NewReader(tarotCommandImg))
+			if err != nil {
+				logger.WithError(err).Error("Fail to upload TarotCommandImg.")
+			} else {
+				replyMsg.Append(img)
+			}
+			c.SendGroupMessage(msg.GroupCode, replyMsg)
+			return
+		}
+		if msgString == "运势预测" {
+			c.SendGroupMessage(msg.GroupCode, drawCard(c, msg.GroupCode, 1))
+			return
+		}
+		if msgString == "塔罗占卜" {
+			c.SendGroupMessage(msg.GroupCode, drawCard(c, msg.GroupCode, 3))
+			return
+		}
+		if msgString[:6] == "塔罗" && len(msgString[6:]) != 0 {
+			countString := msgString[6:]
+			count, err := strconv.ParseInt(countString, 10, 64)
+			if err != nil {
+				return
+			}
+			if count <= 0 {
+				return
+			}
+			if count > 8 {
+				c.SendGroupMessage(msg.GroupCode, simpleText("最多只能抽 8 张塔罗牌。"))
+				return
+			}
+			c.SendGroupMessage(msg.GroupCode, drawCard(c, msg.GroupCode, int(count)))
+			return
+		}
+	})
+}
+
+// Start 此函数会新开携程进行调用
+// ```go
+//
+//	go exampleModule.Start()
+//
+// ```
+// 可以利用此部分进行后台操作
+// 如 http 服务器等等
+func (t *tarot) Start(b *bot.Bot) {
+}
+
+// Stop 结束部分
+// 一般调用此函数时，程序接收到 os.Interrupt 信号
+// 即将退出
+// 在此处应该释放相应的资源或者对状态进行保存
+func (t *tarot) Stop(b *bot.Bot, wg *sync.WaitGroup) {
+	// 别忘了解锁
+	defer wg.Done()
+}
+
+func drawCard(c *client.QQClient, groupCode int64, number int) *message.SendingMessage {
+	theme := "classic"
+	if (rand.Int() % 3) == 0 {
+		theme = "bilibili"
+	}
+	deckPath := path.Join("./assets/deck/", theme)
+	cardImages, err := fs.ReadDir(deckEmbedFS, deckPath)
+	if err != nil {
+		logger.WithError(err).Error("Fail to read cardImages.")
+		return simpleText("发生错误，无法读取塔罗图片。")
+	}
+	cardImagePaths := make([]string, 0, 78)
+	for _, card := range cardImages {
+		if !card.IsDir() {
+			_imgPath := path.Join(deckPath, card.Name())
+			cardImagePaths = append(cardImagePaths, _imgPath)
+		}
+	}
+	cardResult := randomDraw(cardImagePaths, number)
+	replyMsg := simpleText("你抽到的卡牌如下：")
+	for _, imgPath := range cardResult {
+		// 读取图片
+		imgData, err := fs.ReadFile(deckEmbedFS, imgPath)
+		if err != nil {
+			logger.WithError(err).Error("Fail to read img.")
+			replyMsg.Append(message.NewText("[ERROR] 读取图片失败\n"))
+		}
+		// 上传图片
+		imgEle, err := uploadImage(c, groupCode, bytes.NewReader(imgData))
+		if err != nil {
+			logger.WithError(err).Error("Fail to upload img.")
+			replyMsg.Append(message.NewText("[ERROR] 上传图片失败\n"))
+		}
+		replyMsg.Append(imgEle)
+	}
+	return replyMsg
+}
+
+// randomDraw 随机不放回抽取
+func randomDraw(s []string, k int) []string {
+	n := len(s)
+	if k > n {
+		k = n
+	}
+
+	result := make([]string, k)
+	for i := 0; i < k; i++ {
+		j := rand.Intn(n-i) + i
+		result[i] = s[j]
+		s[i], s[j] = s[j], s[i]
+	}
+
+	return result
+}
+
+func simpleText(text string) *message.SendingMessage {
+	return message.NewSendingMessage().Append(message.NewText(text))
+}
+
+func uploadImage(c *client.QQClient, groupCode int64, img io.ReadSeeker) (*message.GroupImageElement, error) {
+	// 尝试上传图片
+	ele, err := c.UploadGroupImage(groupCode, img)
+	// 发生错误时重试 3 次，否则报错
+	for i := 0; i < 3 && err != nil; i++ {
+		ele, err = c.UploadGroupImage(groupCode, img)
+	}
+	if err != nil {
+		logger.WithError(err).Error("Unable to upload image.")
+		return nil, err
+	}
+	return ele, nil
+}
